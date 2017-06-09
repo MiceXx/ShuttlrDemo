31d2397f9b9d5ae0f1bd6896fb67efc6c2ffbc8f
Michael Xu <accmxx@gmail.com>
2017-06-09 01:44:22 -0400

    add screens

---
+++ app/src/main/java/com/accmxxgmail/shuttlrdemo/SessionManagement.java
@@ -0,0 +1,68 @@
+package com.accmxxgmail.shuttlrdemo;
+
+import java.util.HashMap;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+public class SessionManagement {
+    SharedPreferences pref;
+    Editor editor;
+    Context _context;
+
+    private static final String IS_LOGIN = "IsLoggedIn";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_EMAIL = "email";
+
+    public SessionManagement(Context context){
+        this._context = context;
+        pref = _context.getSharedPreferences("CHECK_LOGIN", 0);
+        editor = pref.edit();
+    }
+
+    public void createLoginSession(String name, String email){
+        // Storing login value as TRUE
+        editor.putBoolean(IS_LOGIN, true);
+        editor.putString(KEY_NAME, name);
+        editor.putString(KEY_EMAIL, email);
+        editor.commit();
+    }
+
+    /**
+     * Check login method wil check user login status
+     * If false it will redirect user to login page
+     * Else won't do anything
+     * */
+    public void checkLogin(){
+        if(!this.isLoggedIn()){
+            Intent i = new Intent(_context, LoginActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            _context.startActivity(i);
+        }
+
+    }
+
+    public HashMap<String, String> getUserDetails(){
+        HashMap<String, String> user = new HashMap<String, String>();
+        user.put(KEY_NAME, pref.getString(KEY_NAME, null));
+        user.put(KEY_EMAIL, pref.getString(KEY_EMAIL, null));
+        return user;
+    }
+
+    public void logoutUser(){
+        editor.clear();
+        editor.commit();
+
+        Intent i = new Intent(_context, LoginActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        _context.startActivity(i);
+    }
+
+    public boolean isLoggedIn(){
+        return pref.getBoolean(IS_LOGIN, false);
+    }
+}
---
+++ app/src/main/java/com/accmxxgmail/shuttlrdemo/UserDetails.java
@@ -0,0 +1,10 @@
+package com.accmxxgmail.shuttlrdemo;
+
+public class UserDetails {
+    static String email = "";
+    static String name = "";
+    static String password = "";
+    static String address = "";
+    static String phone = "";
+    static String company = "";
+}
